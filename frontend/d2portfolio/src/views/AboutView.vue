78ae1d87c16aef4b6ef649389445ee44bae2019f
dmdiazf <newdmdiazf@gmail.com>
2023-04-06 20:59:41 -0500

    Configuracion de TailwindCSS para el proyecto FrontEnd

--- frontend/d2portfolio/src/views/AboutView.vue
+++ frontend/d2portfolio/src/views/AboutView.vue
@@ -1,15 +1,11 @@
+<script setup>
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold">This is the about view.</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
--- frontend/d2portfolio/src/views/HomeView.vue
+++ frontend/d2portfolio/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold">This is the home view.</h1>
+  </div>
 </template>
