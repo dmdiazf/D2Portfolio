8b807e6dfb784eb759c39520eeb77168fc7618c4
dmdiazf <newdmdiazf@gmail.com>
2023-04-06 22:09:46 -0500

    Se agregan RouterLinks al componente NavBar

--- frontend/d2portfolio/src/components/NavBar.vue
+++ frontend/d2portfolio/src/components/NavBar.vue
@@ -4,7 +4,7 @@
 
 <template>
 
-<nav class="bg-gray-800">
+<nav class="bg-gray-800 rounded-t-lg shadow-lg">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -37,7 +37,10 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+            <router-link to="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Home</router-link>
+            <router-link to="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</router-link>
+            
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
 
